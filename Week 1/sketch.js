efe0a61039f0306938ab52f8ef2fd9bd572215ce
Shannon <hanlon.s@husky.neu.edu>
2018-01-18 21:28:07 -0500

    Started HW by Accident
    Here's the start.

---
+++ Week 1/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 480);
+  background(255);
+  ellipse(75,75,100,100);
+  line(75,125,75,220);
+  strokeWeight(2);
+  line(60,50,60,60);
+  line(90,50,90,60);
+  line(75,75,75,80);
+  bezier(60,100,75,110,75,110,85,100);
+  strokeWeight(1);
+  line(75,200,150,100);
+  line(75,200,150,20)
+}
+
